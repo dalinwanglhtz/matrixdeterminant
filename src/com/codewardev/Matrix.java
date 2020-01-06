9b79bb31b474bdb4b03bd5b4a7fc94efa09d64ac
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-06 22:12:02 +1100

    refined working cut

--- src/com/codewardev/Matrix.java
+++ src/com/codewardev/Matrix.java
@@ -5,17 +5,11 @@ package com.codewardev;
 public class Matrix {
 
 	public static int determinant(int[][] matrix) {
-		if(matrix.length == 1) {
-			return matrix[0][0];
-		}
+		if(matrix.length == 1) return matrix[0][0];
 		
 		int dm = 0;
 		for(int i=0; i<matrix[0].length; i++) {
-			if(i%2 == 0) {
-				dm += matrix[0][i]*determinant(getJuniorMatrix(i, matrix));
-			} else {
-				dm -= matrix[0][i]*determinant(getJuniorMatrix(i, matrix));
-			}
+			dm += (i%2 == 0? 1 : -1)*matrix[0][i]*determinant(getJuniorMatrix(i, matrix));
 		}
 
 		return dm;
@@ -28,10 +22,7 @@ public class Matrix {
 		int index = 0;
 		for(int j=1; j<matrix.length; j++) {
 			for(int k=0; k<matrix.length; k++) {
-				if(k!=i) {
-					junior[j-1][index] = matrix[j][k];
-					index++;
-				}
+				if(k!=i) junior[j-1][index++] = matrix[j][k];
 			}
 			index=0;
 		}
